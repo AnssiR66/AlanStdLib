7c41818f34c00eba45370b5642fc390e93f37ecc
tajmone <tajmone@libero.it>
2020-09-23 01:17:08 +0200

    StdLib Man Quick Builder
    Create `extras_src/manual/html-build.sh`, a script to quickly update
    the StdLib Manual without processing all the ALAN source examples, in
    order to allow quick previews of changes to the ADoc sources.
    
    The script was quickly setup in a rather hackish way, and will soon be
    replaced with a better version (all toolachain scripts will).

---
+++ extras_src/manual/html-build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# manual/html-build.sh    v0.0.1 | 2020/09/23 | by Tristano Ajmone, MIT License.
+################################################################################
+#                                                                              #
+#                           BUILD STDLIB MANUAL HTML                           #
+#                                                                              #
+################################################################################
+# Rebuild the StdLib Man (HTML) from AsciiDoc, without rebuilding the examples.
+
+# NOTE: This is a temporary script, it was setup quickly and in a rather hackish 
+#       way just to be able to quickly update the HTML document without having
+# 		to run "../update.sh", which compiles all the ALAN adventures at each
+# 		run and takes ages. It will soon be replaced with a better script, as
+# 		all the scripts will be, in due time ...
+
+################################################################################
+#                               SETUP & SETTINGS                               #
+################################################################################
+source ../../assets/sh/init-env.sh  # Initialize work environment
+source _print-funcs.sh           # Ornamental print functions
+source _build-funcs.sh           # Build and deploy functions
+
+export invoker="manual/html-build.sh"   # Used by some external scripts
+################################################################################
+#                                BANNER & INTRO                                #
+################################################################################
+printBanner "Build StdLib Manual HTML"
+
+outDir=../../extras/manual
+adocDir="../adoc"
+hamlDir="$adocDir/haml"
+utfDir="utf8/manual"
+
+for sourcefile in ./*.asciidoc ; do
+adoc2html $sourcefile
+if [ $? -ne 0 ] ; then
+  printAborting ; exit 1
+fi
+done
+
+printFinished
+exit
+
+# ------------------------------------------------------------------------------
+# The MIT License
+#
+# Copyright (c) 2019 Tristano Ajmone: <tajmone@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+# EOF #
